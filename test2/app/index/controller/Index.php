8c51ac6bb93572796291401a56e17d6a38e92933
chenyi-sun <605244701@qq.com>
2017-12-10 16:26:45 +0800

    add

--- test2/app/index/controller/Index.php
+++ test2/app/index/controller/Index.php
@@ -4,13 +4,21 @@ use app\common\controller\Index as User;
 class Index extends User
 {
     public function __construct(){
+        config('dddd','ssss');
         
     }
     public function index()
     {
         //$ss = new base();
     //    return $this->ask();
-         dump(config());
+         dump(config('dddd'));
+        $res = [
+            'sss' => 'ddd'
+        ];
+        $res2 = [
+            'sss' => 'dsss'
+        ];
+        dump(array_merge($res, $res2));
     }
     public function hello($name="ddddd"){
         return 'Hello!'.$name;
--- test2/conf/config.php
+++ test2/conf/config.php
@@ -1,7 +1,8 @@
 <?php
     return [
+        'app_status' => 'home', //home/office
         'namess' => 'dddd',
         'name_lsk' => 'kdksks',
         'ndnmns' => 'sssssfdssd'
-    ]
+    ];
 ?>
